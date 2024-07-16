2fcc6170394ccc43b844ba5c2f28dc40fa98c36d
TEGGOUR Abdenour <ateggour@sqli.com>
2024-07-16 17:48:54 +0100

    courses table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,11 +1,26 @@
-"use client"
 import { Button } from "@/components/ui/button"
+import { Course } from "@prisma/client";
 import Link from "next/link"
+import { DataTable } from "./_components/dataTable";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const {userId} = auth()
+  if(!userId){
+    return redirect('/')
+  }
+  const courses = await db.course.findMany({where:{userId},orderBy:{createdAt:"desc"}})
   return (
-    <div className="p-6"><Link href="/teacher/create"><Button>New Course</Button></Link></div>
-  )
-}
+    <div className="p-6">
+      {/* <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link> */}
+      <DataTable columns={columns} data={courses} />
+    </div>
+  );
+};
 
 export default CoursesPage
