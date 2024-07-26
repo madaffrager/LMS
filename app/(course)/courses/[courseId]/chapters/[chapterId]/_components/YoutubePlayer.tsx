45c84da722b7e785da62ffc1b53d388cf045888f
TEGGOUR Abdenour <ateggour@sqli.com>
2024-07-26 20:38:13 +0100

    update youtube iframe

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/YoutubePlayer.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/YoutubePlayer.tsx
@@ -1,5 +1,3 @@
 export function YoutubePlayer({url}:{url:string}) {
-  return (
-    <iframe width="560" height="315" src={url} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  );
+  return <iframe className="w-full h-[300px]" src={url}></iframe>;
 }
