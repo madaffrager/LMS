3494e97c2ecad940b6e81b8fe531008fbe9bbee1
TEGGOUR Abdenour <ateggour@sqli.com>
2024-07-26 20:43:21 +0100

    update iframe height

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/YoutubePlayer.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/YoutubePlayer.tsx
@@ -1,3 +1,3 @@
 export function YoutubePlayer({url}:{url:string}) {
-  return <iframe className="w-full h-[300px]" src={url}></iframe>;
+  return <iframe className="w-full h-[476px]" src={url}></iframe>;
 }
